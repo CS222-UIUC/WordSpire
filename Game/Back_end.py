c3ae73b37823b6149a86812cb5ba23f6b0260ee9
Phillip Nakamura <pknakamura818@gmail.com>
2024-10-04 16:12:25 -0500

    Added comments

--- Game/Back_end.py
+++ Game/Back_end.py
@@ -1,46 +1,119 @@
-
 import random
 
+#default letters and their value
 default_letter_bag = ["A","A","A","A","A","A","A","A","A","B","B","C","C","D","D","D","D","E","E","E","E","E","E","E","E","E","E","E","E","F","F","G","G","G","H","H","I","I","I","I","I","I","I","I","I","J","K","L","L","L","L","M","M","N","N","N","N","N","N","O","O","O","O","O","O","O","O","P","P","Q","R","R","R","R","R","R","S","S","S","S","T","T","T","T","T","T","U","U","U","U","V","V","W","W","X","Y","Y","Z"]
 default_letter_values = {'A': 1, 'B': 3, 'C': 3, 'D': 2, 'E': 1, 'F': 4, 'G': 2, 'H': 4, 'I': 1, 
                          'J': 8, 'K': 5, 'L': 1, 'M': 3, 'N': 1, 'O': 1, 'P': 3, 'Q': 10, 'R': 1, 
                          'S': 1, 'T': 1, 'U': 1, 'V': 4, 'W': 4, 'X': 8, 'Y': 4, 'Z': 10}
 
 class Game:
-    def __init__(self, board_size = (7,7), rack_size = 7, letter_bag = default_letter_bag, letter_values = default_letter_values, mode = "local_mult"):        
-        self.board = [["*" for i in range(board_size[0])]for i in range(board_size[1])]
+    #all arguments have a defualt value but can be overwritten
+    def __init__(self, board_size = (7,7), rack_size = 7, letter_bag = default_letter_bag, letter_values = default_letter_values, mode = "local_mult"): 
+        """Innitialization function
+        Note: all arguments have a defualt value but can be overwritten
+
+        Args:
+            board_size (tuple[2]): 1D tupple of length 2 containing width and height respectively
+            rack_size  (int): number of letters player can choose from on the letter rack
+            letter_bag (list): list of starting letters
+            letter_values (dict): a dictionary mapping all letters to their point value
+            mode (str): describes how the game should run ["single","local_mult","online_mult","vs_bot"]
+
+        Return:
+            Void
+        """
+
+        #create board with list comprehension
+        self.width = board_size[1]
+        self.height = board_size[0]
+        self.board = [["*" for i in range(self.width)]for i in range(self.height)]
+        
+        #create random letter rack and 
         shuffled_letters = random.shuffle(letter_bag)
-        self.letter_bag = shuffled_letters[rack_size:]
-        self.rack = shuffled_letters[0:rack_size]
+        self.rack_size = rack_size
+        self.letter_bag = shuffled_letters[self.rack_size:]
+        self.rack = shuffled_letters[0:self.rack_size]
+
+        #other misc info
         self.mode = mode
         self.p1_score = 0
         self.p2_score = 0
         self.turn = True
 
     def get_board(self):
+        """Board getter function
+
+        Return:
+            board (2D-list): 2D array of strings representing the board
+        """
         return self.board
     
     def get_rack(self):
+        """Rack getter function
+
+        Return:
+            rack (list): list of strings representing current letters on the rack
+        """
         return self.rack
     
     def get_scores(self):
+        """Score getter function
+
+        Return:
+            score (tuple[2]): tuple of length 2 containing score of player 1 and player 2 respectively
+        """
         return (self.p1_score, self.p2_score)
         
     def get_turn(self):
+        """Turn getter function
+
+        Return:
+            turn (int): 0 if player 1, 1 if player 2, in single player always 0
+        """
         return int(self.turn)
 
     def place_piece(self, rack_idx, col_idx):
+        """Function to place a new tile from the tiles on the letter rack
+
+        Note: Scoring not yet implemented
+            rack_idx (int): an int between 0 and rack_size
+            col_idx (int): an int between 0 and board_width
+
+        Return:
+            status_code (int): 0 if successful, 1 if column is full, 2 if outside board, 3 if outside rack 
+        """
+        
+        #ensure valid input values
+        if col_idx >= self.width or col_idx < 0:
+            return 2
+        if rack_idx >= self.rack_size or rack_idx < 0:
+            return 3
+
+        #main placement loop
         for row in self.board[::-1]:
-            if row[col_idx] == "*":
-                row[col_idx] = self.rack[rack_idx]
+
+            #find lowest empty point in col
+            if row[col_idx] == "*": 
+
+                #place the piece
+                row[col_idx] = self.rack[rack_idx] 
+                
+                #refill letter rack
                 self.rack[rack_idx] = self.letter_bag[0]
                 self.letter_bag = self.letter_bag[1:]
-                self.turn = not self.turn
-                return 0
 
-        return 1
+                #score
+                #to-do
+                
+                #change_turn
+                if self.mode == "local_mult" or self.mode == "online_mult":
+                    self.turn = not self.turn
+                
+                return 0 #returns 0 as succeeded
 
+        return 1 #returns 1 error code for column full
 
+    # unused:
     # def get_col(self, col_idx):
     #     col = []
     #     for row in self.board:
