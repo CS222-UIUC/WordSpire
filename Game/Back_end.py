f89b35ea89caf6a00b2d79b263e6688f6a608a47
Phillip Nakamura <pknakamura818@gmail.com>
2024-10-04 15:20:30 -0500

    addeded accessor funcitons and new fields to init

--- Game/Back_end.py
+++ Game/Back_end.py
@@ -8,13 +8,29 @@ class Game:
         self.letter_bag = shuffled_letters[rack_size:]
         self.rack = shuffled_letters[0:rack_size]
         self.mode = mode
+        self.p1_score = 0
+        self.p2_score = 0
+        self.turn = True
+
+    def get_board(self):
+        return self.board
+    
+    def get_rack(self):
+        return self.rack
+    
+    def get_scores(self):
+        return (self.p1_score, self.p2_score)
+        
+    def get_turn(self):
+        return int(self.turn)
 
     def place_piece(self, rack_idx, col_idx):
         for row in self.board[::-1]:
             if row[col_idx] == "*":
                 row[col_idx] = self.rack[rack_idx]
                 self.rack[rack_idx] = self.letter_bag[0]
-                self.letter_bag = self.letter_bag[1:0]
+                self.letter_bag = self.letter_bag[1:]
+                self.turn = not self.turn
                 return 0
 
         return 1
