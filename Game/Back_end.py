4e3e662cbc52e1813c5278785bf7f821b950ffd6
Phillip Nakamura <pknakamura818@gmail.com>
2024-10-04 16:17:19 -0500

    intalled and used pep8

--- Game/Back_end.py
+++ Game/Back_end.py
@@ -1,14 +1,16 @@
 import random
 
-#default letters and their value
-default_letter_bag = ["A","A","A","A","A","A","A","A","A","B","B","C","C","D","D","D","D","E","E","E","E","E","E","E","E","E","E","E","E","F","F","G","G","G","H","H","I","I","I","I","I","I","I","I","I","J","K","L","L","L","L","M","M","N","N","N","N","N","N","O","O","O","O","O","O","O","O","P","P","Q","R","R","R","R","R","R","S","S","S","S","T","T","T","T","T","T","U","U","U","U","V","V","W","W","X","Y","Y","Z"]
-default_letter_values = {'A': 1, 'B': 3, 'C': 3, 'D': 2, 'E': 1, 'F': 4, 'G': 2, 'H': 4, 'I': 1, 
-                         'J': 8, 'K': 5, 'L': 1, 'M': 3, 'N': 1, 'O': 1, 'P': 3, 'Q': 10, 'R': 1, 
+# default letters and their value
+default_letter_bag = ["A", "A", "A", "A", "A", "A", "A", "A", "A", "B", "B", "C", "C", "D", "D", "D", "D", "E", "E", "E", "E", "E", "E", "E", "E", "E", "E", "E", "E", "F", "F", "G", "G", "G", "H", "H", "I", "I", "I", "I", "I", "I", "I", "I", "I", "J", "K",
+                      "L", "L", "L", "L", "M", "M", "N", "N", "N", "N", "N", "N", "O", "O", "O", "O", "O", "O", "O", "O", "P", "P", "Q", "R", "R", "R", "R", "R", "R", "S", "S", "S", "S", "T", "T", "T", "T", "T", "T", "U", "U", "U", "U", "V", "V", "W", "W", "X", "Y", "Y", "Z"]
+default_letter_values = {'A': 1, 'B': 3, 'C': 3, 'D': 2, 'E': 1, 'F': 4, 'G': 2, 'H': 4, 'I': 1,
+                         'J': 8, 'K': 5, 'L': 1, 'M': 3, 'N': 1, 'O': 1, 'P': 3, 'Q': 10, 'R': 1,
                          'S': 1, 'T': 1, 'U': 1, 'V': 4, 'W': 4, 'X': 8, 'Y': 4, 'Z': 10}
 
+
 class Game:
-    #all arguments have a defualt value but can be overwritten
-    def __init__(self, board_size = (7,7), rack_size = 7, letter_bag = default_letter_bag, letter_values = default_letter_values, mode = "local_mult"): 
+    # all arguments have a defualt value but can be overwritten
+    def __init__(self, board_size=(7, 7), rack_size=7, letter_bag=default_letter_bag, letter_values=default_letter_values, mode="local_mult"):
         """Innitialization function
         Note: all arguments have a defualt value but can be overwritten
 
@@ -23,18 +25,19 @@ class Game:
             Void
         """
 
-        #create board with list comprehension
+        # create board with list comprehension
         self.width = board_size[1]
         self.height = board_size[0]
-        self.board = [["*" for i in range(self.width)]for i in range(self.height)]
-        
-        #create random letter rack and 
+        self.board = [["*" for i in range(self.width)]
+                      for i in range(self.height)]
+
+        # create random letter rack and
         shuffled_letters = random.shuffle(letter_bag)
         self.rack_size = rack_size
         self.letter_bag = shuffled_letters[self.rack_size:]
         self.rack = shuffled_letters[0:self.rack_size]
 
-        #other misc info
+        # other misc info
         self.mode = mode
         self.p1_score = 0
         self.p2_score = 0
@@ -47,7 +50,7 @@ class Game:
             board (2D-list): 2D array of strings representing the board
         """
         return self.board
-    
+
     def get_rack(self):
         """Rack getter function
 
@@ -55,7 +58,7 @@ class Game:
             rack (list): list of strings representing current letters on the rack
         """
         return self.rack
-    
+
     def get_scores(self):
         """Score getter function
 
@@ -63,7 +66,7 @@ class Game:
             score (tuple[2]): tuple of length 2 containing score of player 1 and player 2 respectively
         """
         return (self.p1_score, self.p2_score)
-        
+
     def get_turn(self):
         """Turn getter function
 
@@ -82,36 +85,36 @@ class Game:
         Return:
             status_code (int): 0 if successful, 1 if column is full, 2 if outside board, 3 if outside rack 
         """
-        
-        #ensure valid input values
+
+        # ensure valid input values
         if col_idx >= self.width or col_idx < 0:
             return 2
         if rack_idx >= self.rack_size or rack_idx < 0:
             return 3
 
-        #main placement loop
+        # main placement loop
         for row in self.board[::-1]:
 
-            #find lowest empty point in col
-            if row[col_idx] == "*": 
+            # find lowest empty point in col
+            if row[col_idx] == "*":
 
-                #place the piece
-                row[col_idx] = self.rack[rack_idx] 
-                
-                #refill letter rack
+                # place the piece
+                row[col_idx] = self.rack[rack_idx]
+
+                # refill letter rack
                 self.rack[rack_idx] = self.letter_bag[0]
                 self.letter_bag = self.letter_bag[1:]
 
-                #score
-                #to-do
-                
-                #change_turn
+                # score
+                # to-do
+
+                # change_turn
                 if self.mode == "local_mult" or self.mode == "online_mult":
                     self.turn = not self.turn
-                
-                return 0 #returns 0 as succeeded
 
-        return 1 #returns 1 error code for column full
+                return 0  # returns 0 as succeeded
+
+        return 1  # returns 1 error code for column full
 
     # unused:
     # def get_col(self, col_idx):
@@ -119,20 +122,3 @@ class Game:
     #     for row in self.board:
     #         col.append(row[col_idx])
     #     return col
-
-
-
-
-    
-
-
-
-
-
-
-
-
-
-
-
-
