79c626881eab62eeec54b8b5b24002fabe2c1024
Phillip Nakamura <pknakamura818@gmail.com>
2024-10-04 14:59:38 -0500

    added innit for game class

--- Game/Back_end.py
+++ Game/Back_end.py
@@ -1,1 +1,25 @@
 
+import random
+
+class Game:
+  def __init__(self, board_size = (7,7), rack_size = 7, letter_bag = "", mode = "local_mult"):
+    self.board = [["*" for i in range(board_size[0])]for i in range(board_size[1])]
+    shuffled_letters = random.shuffle(letter_bag)
+    self.letter_bag = shuffled_letters[rack_size:]
+    self.rack = shuffled_letters[0:rack_size]
+    self.mode = mode
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
